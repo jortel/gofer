34fb3e5483e53fe9a9f8a26815c8c3a54a9f2502
Jeff Ortel <jortel@redhat.com>
2011-06-16 08:05:33 -0500

    License as: LGPLv2.

--- src/gofer/__init__.py
+++ src/gofer/__init__.py
@@ -1,16 +1,16 @@
 #
-# Copyright (c) 2010 Red Hat, Inc.
+# Copyright (c) 2011 Red Hat, Inc.
 #
-# This software is licensed to you under the GNU General Public License,
-# version 2 (GPLv2). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
-# along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+# This software is licensed to you under the GNU Lesser General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (LGPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of LGPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/lgpl-2.0.txt.
 #
-# Red Hat trademarks are not licensed under GPLv2. No permission is
-# granted to use or replicate Red Hat trademarks that are incorporated
-# in this software or its documentation.
+# Jeff Ortel <jortel@redhat.com>
 #
 
 from threading import RLock
