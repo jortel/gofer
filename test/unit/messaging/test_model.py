51193c445662a7ff4acbd5f85d1003311e60b1f6
Jeff Ortel <jortel@redhat.com>
2014-12-01 08:19:24 -0600

    add messaging model unit tests.

--- test/unit/messaging/test_model.py
+++ test/unit/messaging/test_model.py
@@ -11,6 +11,73 @@
 
 from unittest import TestCase
 
+from gofer.messaging.model import VERSION, Document, validate
+from gofer.messaging.model import InvalidDocument, InvalidVersion
 
-class Test(TestCase):
-    pass
+
+class TestExceptions(TestCase):
+
+    def test_invalid_document(self):
+        code = '1'
+        description = '2'
+        document = '3'
+        details = '4'
+
+        # test
+        exception = InvalidDocument(code, description, document, details=details)
+
+        # validation
+        self.assertEqual(exception.code, code)
+        self.assertEqual(exception.args, ('2 : 4',))
+        self.assertEqual(exception.document, document)
+        self.assertEqual(exception.details, details)
+
+    def test_invalid_version(self):
+        document = Document(version='1.0')
+        details = '4'
+
+        # test
+        exception = InvalidVersion(document, details)
+
+        # validation
+        self.assertEqual(exception.code, InvalidVersion.CODE)
+        self.assertEqual(exception.args, ('%s : 4' % InvalidVersion.DESCRIPTION,))
+        self.assertEqual(exception.document, document.dump())
+        self.assertEqual(exception.details, details)
+
+
+class TestValidate(TestCase):
+
+    def test_valid(self):
+        document = Document(version=VERSION)
+        validate(document)
+
+    def test_invalid(self):
+        document = Document(version=VERSION+'.0')
+        self.assertRaises(InvalidVersion, validate, document)
+
+
+class TestDocument(TestCase):
+
+    def test_load(self):
+        s = '{"A": 1}'
+        document = Document()
+        document.load(s)
+        self.assertEqual(document.__dict__, {'A': 1})
+
+    def test_dump(self):
+        document = Document(
+            A=1,
+            B=2,
+            C=Document(a=1, b=2),
+            D=dict(x=10, y=20),
+            E=[1, Document(), dict()],
+            F=10,
+            G='howdy',
+            H=True,
+        )
+        s = document.dump()
+        self.assertEqual(
+            s,
+            '{"A": 1, "B": 2, "C": {"a": 1, "b": 2}, "D": {"x": 10, "y": 20}, '
+            '"E": [1, {}, {}], "F": 10, "G": "howdy", "H": true}')
